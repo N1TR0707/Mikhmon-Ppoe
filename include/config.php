fa32c15fd7ee446b3c18037bfbeaec0cc919de15
Ibrahim Akbar <111202214417@mhs.dinus.ac.id>
2025-01-25 12:43:37 +0700

    first commit

---
+++ include/config.php
@@ -0,0 +1,8 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<diher','mikhmon>|>aWNlbA==');
+
+
+
+
+$data['asd'] = array ('1'=>'asd!86.38.247.211:33152','asd@|@admin','asd#|#mZynn5KgrJagnw==','asd%ad','asd^sad','asd&Rp','asd*10','asd(11','asd)','asd=10','asd@!@disable');
